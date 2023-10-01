39925148bedc4e3dce1f94bcc11466333bade543
YabseraBogale <yabserabogalercd30932013@gmail.com>
2023-10-01 17:42:19 +0300

    now most of it should work

--- pig_game/static/app.js
+++ pig_game/static/app.js
@@ -1,10 +1,6 @@
 function newGame(){
-    let p1=document.getElementById('p1')
-    p1.innerText="0"
-    let p2=document.getElementById('p2')
-    p2.innerText="0"
+    location.reload();
 }
-
 function roll_player1(){
     let num=Math.floor(Math.random()*(7-1)+1)
     let added=Math.floor(Math.random()*(100-1)+1)
@@ -12,18 +8,22 @@ function roll_player1(){
     rolled.innerText=num.toString()
     let p1_price=Number(document.getElementById('p1').innerText)
     if (p1_price>=100){
-
         p1.innerText="You Win"
         let btn1=document.getElementById("btn1")
         let btn2=document.getElementById("btn2")
         btn1.disabled=true
         btn2.disabled=true
-    
     } else if(num!=1 && p1_price<100){
-
         let p1=document.getElementById('p1')
-        p1.innerText=(p1_price+added).toString()
-    
+        if (p1_price+added>=100){
+            p1.innerText=(p1_price+added).toString()+ " You Win"
+            let btn1=document.getElementById("btn1")
+            let btn2=document.getElementById("btn2")
+            btn2.disabled=true
+            btn1.disabled=true
+        } else{
+            p1.innerText=(p1_price+added).toString()
+        }
     }
     else{
         let p1=document.getElementById('p1')
@@ -35,7 +35,6 @@ function roll_player1(){
         document.getElementById("turn").innerText="player 2"
     }
 }
-
 function hold_player(){
     let turn =document.getElementById("turn").innerText
     if (turn == "player 1"){
@@ -52,9 +51,7 @@ function hold_player(){
         btn1.disabled=false
         document.getElementById("turn").innerText="player 1"
     }
-
 }
-
 function roll_player2(){
     let num=Math.floor(Math.random()*(7-1)+1)
     let added=Math.floor(Math.random()*(100-1)+1)
@@ -71,8 +68,16 @@ function roll_player2(){
 
     } else if(num!=1 && p2_price<100){
         let p2=document.getElementById('p2')
-        p2.innerText=(p2_price+added).toString()
-        console.log(p2.innerText);
+        if (p2_price+added>=100){
+            p2.innerText=(p2_price+added).toString()+ " You Win"
+            let btn1=document.getElementById("btn1")
+            let btn2=document.getElementById("btn2")
+            btn2.disabled=true
+            btn1.disabled=true
+            
+        } else{
+            p2.innerText=(p2_price+added).toString()
+        }
     }
     else{
         let p2=document.getElementById('p2')
