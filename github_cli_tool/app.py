548fe522b6bf8e1b8ca565d7e1a27138b483f03b
YabseraBogale <yabserabogalercd30932013@gmail.com>
2023-10-04 17:54:21 +0300

    hell yeah

--- github_cli_tool/app.py
+++ github_cli_tool/app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask,url_for,render_template
 
 app=Flask(__name__)
 
 @app.route('/')
 def index():
-	return "hello Github"
+	return render_template('github.html')
 
 
 if __name__=="__main__":
