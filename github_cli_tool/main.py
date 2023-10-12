0d39e5a64d303c7f7a0a0d6f02da9bb165ee4b7c
YabseraBogale <yabserabogalercd30932013@gmail.com>
2023-10-12 16:34:35 +0300

    approval

--- github_cli_tool/main.py
+++ github_cli_tool/main.py
@@ -13,17 +13,17 @@ class Commit():
             self.database.giveMeLengthOfWordGiveYouFile(fileLength)
         return "Done"
     
-    def gitcommit(self,msg):
+    def gitcommit(self,msg,number_of_time_runnig):
         print(os.system("git status"))
         print(os.system("git add ."))
         print(os.system(f"git commit -m '{msg}'"))
         print(os.system("git push"))
+        print(f"Number of commit is at {number_of_time_runnig}")
 
     def giveMeCommitNumber(self,numberOfCommit):
         for i in range(0,numberOfCommit):
             self.howManyFilesAndNumberOfWord(1,100)
             msg=self.database.seeWordWithId(random.randint(0,500))
-            self.gitcommit(msg)
-            print(f"Number of commit is at {i}")
+            self.gitcommit(msg,i)
         self.database.closeDatabase()
         return "Done"
