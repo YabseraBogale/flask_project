c301c5aadd4d3e77a4114c385dfc3dc3f3710003
YabseraBogale <yabserabogalercd30932013@gmail.com>
2023-10-22 18:22:03 +0300

    ok

--- github_cli_tool/app.py
+++ github_cli_tool/app.py
@@ -1,10 +1,13 @@
 from flask import Flask,url_for,render_template
-
+from main import Commit
 app=Flask(__name__)
+
 
 @app.route('/')
 def index():
-	return render_template('github.html')
+	git=Commit()
+	ok=git.onlyGitCommit()
+	return render_template('github.html',ok=ok)
 
 
 if __name__=="__main__":
