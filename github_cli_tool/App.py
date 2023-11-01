b4c33142c1b6430eac5f7806e9452950039dc984
YabseraBogale <yabserabogalercd30932013@gmail.com>
2023-11-01 16:15:20 +0300

    ok

--- github_cli_tool/App.py
+++ github_cli_tool/App.py
@@ -1,10 +1,13 @@
 from flask import Flask,render_template,url_for
+from Main import Commit
 
 app=Flask(__name__)
+commit=Commit()
 
 @app.route('/')
 def index():
-	return render_template('github.html')
+	ok=commit.onlyGitCommitAll()
+	return render_template('github.html',ok=ok)
 
 
 
---
+++ github_cli_tool/Main.py
@@ -0,0 +1,11 @@
+import os
+
+class Commit():
+	def onlyGitCommitAll(self):
+		self.status=os.system("git status")
+		if self.status==0:
+			os.system("git add .")
+			os.system("git commit -m 'ok'")
+			os.system("git push")
+			return "Operation complete"
+		return "Problem"
