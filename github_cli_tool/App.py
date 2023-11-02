c59cb80cdb8f53ae888cc6882a4fa3a4b5444328
YabseraBogale <yabserabogalercd30932013@gmail.com>
2023-11-02 17:41:49 +0300

    ok installed kate the lsp worked but the linter have problem

--- github_cli_tool/App.py
+++ github_cli_tool/App.py
@@ -1,16 +1,15 @@
-from flask import Flask,render_template,url_for
+from flask import Flask, render_template, url_for
 from Main import Commit
 
-app=Flask(__name__)
-commit=Commit()
+app = Flask(__name__)
+commit = Commit()
 
-@app.route('/')
+
+@app.route("/")
 def index():
-	ok=commit.onlyGitCommitAll()
-	return render_template('github.html',ok=ok)
+    ok = commit.onlyGitCommitAll()
+    return render_template("github.html", ok=ok)
 
 
-
-
-if __name__=="__main__":
-	app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
