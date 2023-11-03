21b86ed9a6934a9edc83ee28043e669621e08456
YabseraBogale <yabserabogalercd30932013@gmail.com>
2023-11-03 08:18:16 +0300

    algor

---
+++ Vaccum Cleaner/main.py
@@ -0,0 +1,19 @@
+from pprint import pprint
+print("Enter \"f\" to Finish")
+q='go'
+lst=[]
+while q!="f":
+	q=input("Enter \"d\" for dirty or \"c\" for clean: ")
+	if q=="d":
+		lst.append([q,"cleaned"])
+	elif q=="c":
+		lst.append([q,"passed"])
+	else:
+		print("Enter the right format")
+		
+print("Finished")
+print("Room Histroy")
+pprint(lst)
+	
+
+
