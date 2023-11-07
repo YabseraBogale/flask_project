c80c4f10da4d267b2903c4aafa7e813a902ac81b
YabseraBogale <yabserabogalercd30932013@gmail.com>
2023-11-07 15:59:05 +0300

    ok

--- simulation/static/app.js
+++ simulation/static/app.js
@@ -1,5 +1,7 @@
 
 let ele=document.querySelector(".robot")
 let position= ele.getBoundingClientRect()
-
-
+//ele.style.position
+console.log(position.left)
+console.log(typeof(position.left))
+console.log(position.left===382)
