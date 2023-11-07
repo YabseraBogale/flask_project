6a1b792a6ed6ed7b78bb8588ef1e16fa18de258f
YabseraBogale <yabserabogalercd30932013@gmail.com>
2023-11-07 16:25:45 +0300

    ok

--- simulation/static/app.js
+++ simulation/static/app.js
@@ -4,8 +4,11 @@ setInterval(function(){
 	let position= ele.getBoundingClientRect()
 	console.log(position);
 	//ele.style=`margin-left:${position.x-5}px;`
-	if(position.x>0){
-		
+	if(position.x<position.right){
+		ele.style=`margin-left:${position.left/2+0.5}px;`
+	}
+	else{
+		ele.style=`margin-left:${position.left/2-0.5}px;`
 	}
 
 },1000);
