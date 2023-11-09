6a0349b20e59d1b6d92bdbf01236d22ae3da4785
YabseraBogale <yabserabogalercd30932013@gmail.com>
2023-11-09 19:25:45 +0300

    ok

--- Simpulation/static/p5/empty-example/sketch.js
+++ Simpulation/static/p5/empty-example/sketch.js
@@ -10,15 +10,43 @@ let x=200;
 let y=200;
 let speed=1;
 let distance=1
-let moitionX=x+10;
-let moitionY=y+10;
-const data=[moitionX,moitionY]
+let moitionX=220;
+let moitionY=220;
+
 function draw() {
   // put drawing code here
-	background(220);
-	let Data={"distance":distance,"moitionX":moitionX,"moitionY":moitionY}
+  	background(220);
 	circle(x, y, 20);
-	console.log(Data);
+	//console.log("moitionX",moitionX,"x",x,"direction",direction);
+	if(x<moitionX && direction==1){
+		x=x+1
+		distance+=1
+		if(x==moitionX){
+			direction=-1
+		}
+	} else if(y<moitionY && direction==-1){
+		y=y+1
+		distance+=1;
+		if(y==moitionY && moitionX==x){
+			direction==1
+			moitionX=moitionX+distance
+		}
+	} else if(x>moitionX && direction==1){
+		x=x-1;
+		distance+=1
+		if(x==moitionX){
+			direction=-1
+			moitionY=moitionY-distance
+		}
+	} else if(y>moitionY && direction==-1){
+		y=y-1
+		distance==1
+		if(y==moitionY){
+			direction=1
+			moitionX=moitionX+distance
+		}
+	}
+	
 	
 	
 	
