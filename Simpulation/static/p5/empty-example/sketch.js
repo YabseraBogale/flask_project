22b1fb3a87f56c29d03cd5bfd43f18a362869fe3
YabseraBogale <yabserabogalercd30932013@gmail.com>
2023-11-09 17:11:07 +0300

    made

--- Simpulation/static/p5/empty-example/sketch.js
+++ Simpulation/static/p5/empty-example/sketch.js
@@ -5,18 +5,22 @@ function setup() {
 
 
 
+let movingObject=[200,200,205,205,1]
+
 function draw() {
   // put drawing code here
 	background(220);
 	
 	// movingObject=[Initail_Point_X,Initail_Point_Y,Step_X,Step_Y,direction]
-	let movingObject=[200,200,5,5,1]
 	circle(movingObject[0], movingObject[1], 20);
-	Motion(movingObject,6);
+	//console.log(movingObject[0]);
+	Motion(movingObject,5);
 
 }
 
 function Motion(movingObject,speed){
+		movingObject[2]=movingObject[0]+speed
+		movingObject[3]=movingObject[1]+speed
 		switch(movingObject){
 			case movingObject[0]<=movingObject[2] && movingObject[1]<=movingObject[3]  && movingObject[4]==1:
 				movingObject[0]=movingObject[0]+speed
@@ -30,18 +34,7 @@ function Motion(movingObject,speed){
 					movingObject[4]=1
 					movingObject[0]=movingObject[0]+movingObject[2]
 				}
-			case movingObject[0]>=movingObject[2] && movingObject[1]==movingObject[3]  && movingObject[4]==1:
-				movingObject[0]=movingObject[0]-speed
-				if(movingObject[0]==movingObject[2]){
-					movingObject[4]=-1
-					movingObject[1]=movingObject[1]+movingObject[3]
-					
-				}
-			case movingObject[0]==movingObject[2] && movingObject[1]>=movingObject[3]  && movingObject[4]==-1:
-				movingObject[1]=movingObject[1]-speed
-				if(movingObject[1]==movingObject[3]){
-					movingObject[4]=1
-				}
+			
 		}
 		
 }
