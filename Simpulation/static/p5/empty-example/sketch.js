40cc55e8c6da4fa9d22a81ba4519c5b969840c41
YabseraBogale <yabserabogalercd30932013@gmail.com>
2023-11-09 20:10:36 +0300

    made

--- Simpulation/static/p5/empty-example/sketch.js
+++ Simpulation/static/p5/empty-example/sketch.js
@@ -18,9 +18,36 @@ function draw() {
   	background(220);
   	fill(0,22,0);
 	circle(x, y, 20);
+	if(x<=moitionX && x<400 && x>0 && direction==1){
+		x+=1
+		distance+=1
+		if(x==moitionX){
+			direction=-1
+		}
+	} else if(y<=moitionY && y<400 && y>0 && direction==-1){
+		y+=1
+		distance+=1
+		if(y==moitionY){
+			direction=1
+		}
+	} else if(x>=moitionX && x<400 && x>0 && direction==1){
+		x-=1
+		distance+=1
+		if(moitionX==x && 0<moitionX-distance){
+			moitionX=moitionX-distance
+		} else if(moitionX==x && 0>moitionX-distance){
+			moitionX=distance-moitionX
+		}
+	} else if(y>=moitionY && y<400 && y>0 && direction==1){
+		y-=1
+		distance+=1
+		if(moitionX==x && 0<moitionY-distance){
+			moitionX=moitionX-distance
+		} else if(moitionY==y && 0>moitionY-distance){
+			moitionY=distance-moitionY
+		}
 	
-	
-	
+	}
 	
 	
 }
