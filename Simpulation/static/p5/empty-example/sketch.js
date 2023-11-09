8ca53a93f81f6a1cbca4ea15b5167038d5557ae0
YabseraBogale <yabserabogalercd30932013@gmail.com>
2023-11-09 19:57:15 +0300

    made

--- Simpulation/static/p5/empty-example/sketch.js
+++ Simpulation/static/p5/empty-example/sketch.js
@@ -19,6 +19,7 @@ function draw() {
 	circle(x, y, 20);
 	//console.log(moitionX,x);
 	//console.log("moitionX",moitionX,"x",x,"direction",direction);
+	console.log(x,moitionX);
 	if(x<=moitionX && x<400 && direction==1){
 		x=x+1
 		distance+=1
@@ -28,21 +29,35 @@ function draw() {
 	} else if(y<=moitionY && y<400 && direction==-1){
 		y=y+1
 		distance+=1;
-		if(y==moitionY && moitionX==x){
+		if(y==moitionY && moitionX==x && moitionX>distance){
 			direction=1
 			moitionX=moitionX-distance
+		} else if(y==moitionY && moitionX==x && moitionX<distance){
+			direction=1
+			moitionX=distance-moitionX
 		}
 		
-	} else if(x>=moitionX && direction==1){
+	} else if(x>=moitionX && x>=0 && direction==1){
 		x=x-1;
 		distance+=1
-		if(x==moitionX && y==moitionY){
+		if(x==moitionX && y==moitionY && moitionY>distance){
 			direction=-1
 			moitionY=moitionY-distance
+		} else if(x==moitionX && y==moitionY && moitionY<distance){
+			direction=-1
+			moitionY=distance-moitionY
+			
 		}
-	} else if(){
-		
+	} else if(y>=moitionY && y>=0 && direction==-1){
+		y=y-1
+		distance+=1
+		if(x==moitionX && y==moitionY){
+			direction=1
+			moitionX=moitionX+distance
+			moitionY=moitionY+distance
+		}
 	}
+	
 	
 	
 }
