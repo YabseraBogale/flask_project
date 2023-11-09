285f3fda60aa6e70a14a4b2270dcad9ff21ba08b
YabseraBogale <yabserabogalercd30932013@gmail.com>
2023-11-09 17:00:23 +0300

    made

--- Simpulation/static/p5/empty-example/sketch.js
+++ Simpulation/static/p5/empty-example/sketch.js
@@ -12,32 +12,36 @@ function draw() {
 	// movingObject=[Initail_Point_X,Initail_Point_Y,Step_X,Step_Y,direction]
 	let movingObject=[200,200,5,5,1]
 	circle(movingObject[0], movingObject[1], 20);
-	switch(movingObject){
-			case movingObject[0]<=movingObject[2] && movingObject[1]<=movingObject[3]  && movingObject[4]==1 && movingObject[0]<=400:
+	Motion(movingObject,6);
+
+}
+
+function Motion(movingObject,speed){
+		switch(movingObject){
+			case movingObject[0]<=movingObject[2] && movingObject[1]<=movingObject[3]  && movingObject[4]==1:
 				movingObject[0]=movingObject[0]+speed
 				console.log(movingObject[0]);
 				if(movingObject[0]==movingObject[2]){
 					movingObject[4]=-1
 				}
-			case movingObject[0]==movingObject[2] && movingObject[1]<=movingObject[3]  && movingObject[4]==-1 && movingObject[1]<=400:
+			case movingObject[0]==movingObject[2] && movingObject[1]<=movingObject[3]  && movingObject[4]==-1:
 				movingObject[1]=movingObject[1]+speed
 				if(movingObject[1]==movingObject[3]){
 					movingObject[4]=1
 					movingObject[0]=movingObject[0]+movingObject[2]
 				}
-			case movingObject[0]>=movingObject[2] && movingObject[1]==movingObject[3]  && movingObject[4]==1 && movingObject[0]>=0:
+			case movingObject[0]>=movingObject[2] && movingObject[1]==movingObject[3]  && movingObject[4]==1:
 				movingObject[0]=movingObject[0]-speed
 				if(movingObject[0]==movingObject[2]){
 					movingObject[4]=-1
 					movingObject[1]=movingObject[1]+movingObject[3]
 					
 				}
-			case movingObject[0]==movingObject[2] && movingObject[1]>=movingObject[3]  && movingObject[4]==-1 movingObject[1]>=0:
+			case movingObject[0]==movingObject[2] && movingObject[1]>=movingObject[3]  && movingObject[4]==-1:
 				movingObject[1]=movingObject[1]-speed
 				if(movingObject[1]==movingObject[3]){
 					movingObject[4]=1
 				}
 		}
-
+		
 }
-
