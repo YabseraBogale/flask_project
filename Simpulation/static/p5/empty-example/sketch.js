f1d9ed203d6c6120114bccaf00ce54fa79a9da5c
YabseraBogale <yabserabogalercd30932013@gmail.com>
2023-11-09 17:44:51 +0300

    made

--- Simpulation/static/p5/empty-example/sketch.js
+++ Simpulation/static/p5/empty-example/sketch.js
@@ -3,37 +3,55 @@ function setup() {
     createCanvas(400, 400);
 }
 
-function moitionMove(pointX,pointY){
-	return [pointX+10,pointY+10]
-}
 
 
-let direction=1
-let x=200
-let y=200
-let moition=moitionMove(x,y)
+let direction=1;
+let x=200;
+let y=200;
+let speed=5;
+let distance=1
+let moitionX=x+10;
+let moitionY=y+10;
+const data=[moitionX,moitionY]
 function draw() {
   // put drawing code here
 	background(220);
+	let Data={"distance":distance,"moitionX":moitionX,"moitionY":moitionY}
 	circle(x, y, 20);
-	moveRobot(x,y,direction,moition);
-	
-}
-
-
-function moveRobot(pointX,pointY,direction,speed,movement){
-	switch(arr){
-		case pointX<=movement[0] && pointX<400 && direction==1:
-			pointX=pointX+speed
-			if(pointX==movement[0]){
-				direction=-1
-			}
-		case pointY<=movement[1] && pointY<400 && direction==-1:
-			pointy=pointY+speed
-			if(pointY==movement[1]){
-				direction=1
-			}
+	console.log(Data);
+	if(x<=moitionX && x<400 && direction==1){
+		x=x+speed;
+		distance+=1
+		if(x==moitionX){
+			direction=-1
+		}
+	} else if(y<=moitionY && y<400 && direction==-1){
+		y=y+speed
+		distance+=1
+		if(y==moitionY){
+			direction=1
+			moitionX=moitionX-distance
+		}
+	} else if(x>=moitionX && x>=0 && direction==1){
+		x=x-speed
+		distance+=1
+		if(moitionX==x){
+			direction=-1
+			moitionY=moitionY-distance
+		}
+	} else if(y>=moitionY && y>=0 && direction==-1){
+		y=y-speed
+		distance+=1
+		if(y==moitionY){
+			direction=1
+			moitionX=data[0]+distance
+			moitionY=data[1]+distance
+		}
 	}
 	
+	
 }
+
+
+
 
