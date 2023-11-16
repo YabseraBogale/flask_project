6c3cd0ff43dd0fe1e540a782028364707ee7abdf
YabseraBogale <yabserabogalercd30932013@gmail.com>
2023-11-16 14:23:01 +0300

    ok

--- reading-json/main.py
+++ reading-json/main.py
@@ -2,6 +2,8 @@ import pandas as pd
 
 import json
 
+list_of_stack=["mysql","go","python","c++","javascript","js","sql","nodejs","react","vue",""]
+
 df=pd.read_csv('msgs_dataset.csv')
 
 for i in range(0,100):
