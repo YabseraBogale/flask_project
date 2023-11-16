8e716c3a3d0a3bb662c171ef279fe72c1879459f
YabseraBogale <yabserabogalercd30932013@gmail.com>
2023-11-16 16:36:58 +0300

    ok

--- reading-json/database.py
+++ reading-json/database.py
@@ -2,4 +2,11 @@ import sqlite3
 
 
 class Software():
-	self.cursor=sqlite3.Connection('database.py') 
+	self.cursor=sqlite3.Connection('database.py')
+	self.pointer=self.connect.cursor()
+	
+	def insertIntoTable(self,id,message,date):
+		statment="insert into Software(id,message,date) values(?,?,?)"
+		self.pointer.execute(statment,(id,message,date))
+		self.cursor.commit()
+		return "Done"
