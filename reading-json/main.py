41b2c5f0f53a57f9bc015e22e5fa88142ad008eb
YabseraBogale <yabserabogalercd30932013@gmail.com>
2023-11-16 16:57:03 +0300

    ok

--- reading-json/main.py
+++ reading-json/main.py
@@ -14,13 +14,14 @@ list_of_stack=["mysql","go","python","c++","javascript",
 df=pd.read_csv('msgs_dataset.csv',low_memory=False)
 count=0
 table=Software()
-for i in range(0,10):
+for i in range(0,len(df["message"])):
 	
 	if df["message"][i].find("#Software_design_and_Development")!=-1:
 		table.insertIntoTable(i,df["message"][i],df["date"][i])
 	
 	if count==100:
 		count=0
+		print(f"Insert up to {i}")
 		sleep(3)
 	else:
 		count+=1
