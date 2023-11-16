e6ce05991fd1130548b6f41cb7b4720c830f9e71
YabseraBogale <yabserabogalercd30932013@gmail.com>
2023-11-16 17:51:10 +0300

    ok

--- reading-json/main.py
+++ reading-json/main.py
@@ -2,7 +2,7 @@ import pandas as pd
 from time import sleep
 from database import Software
 import json
-from flask import Flask,render_template,url_for
+
 list_of_stack=["mysql","go","python","c++","javascript",
 				"js","sql","nodejs","react","vue","php",
 				"c#",".net","django","flask","reactjs",
@@ -11,15 +11,18 @@ list_of_stack=["mysql","go","python","c++","javascript",
 				"boot","css","bootstrap","tailwind","html"
 				"html5","saas","node"]
 
-
+df=pd.read_csv('msgs_dataset.csv',low_memory=False)
+count=0
 table=Software()
+for i in range(0,len(df["message"])):
+	
+	if str(df["message"][i]).find("#Software_design_and_Development")!=-1:
+		table.insertIntoTable(i,df["message"][i],df["date"][i])
+	
+	if count==100:
+		count=0
+		print(f"Insert up to {i}")
+		sleep(3)
+	else:
+		count+=1
 
-app=Flask(__name__)
-
-@app.route('/')
-def index():
-	return render_template('index.html')
-
-
-if __name__=="__main__":
-	app.run(debug=True)
