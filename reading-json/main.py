a39e39c24f6f1851dffc81b6bc46bacddf07385c
YabseraBogale <yabserabogalercd30932013@gmail.com>
2023-11-16 15:14:06 +0300

    ok

--- reading-json/main.py
+++ reading-json/main.py
@@ -10,12 +10,16 @@ list_of_stack=["mysql","go","python","c++","javascript",
 				"boot","css","bootstrap","tailwind","html"
 				"html5","saas","node"]
 
-df=pd.read_csv('msgs_dataset.csv')
+df=pd.read_csv('msgs_dataset.csv',low_memory=False)
 count=0
-for i in range(0,len(df["message"])):
-	print(type(df["message"][i]))
-	if count==100:
+for i in range(0,100):
+	
+	if df["message"][i].find("#Software_design_and_Development")!=-1:
+		print(df["date"])
+	
+	if count==3:
 		count=0
 		sleep(3)
 	else:
 		count+=1
+
