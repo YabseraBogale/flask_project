37510c389177ed395bfd15cdb7ae8cbfc16c532d
YabseraBogale <yabserabogalercd30932013@gmail.com>
2023-11-16 14:45:43 +0300

    ok

--- reading-json/main.py
+++ reading-json/main.py
@@ -5,9 +5,12 @@ import json
 list_of_stack=["mysql","go","python","c++","javascript",
 				"js","sql","nodejs","react","vue","php",
 				"c#",".net","django","flask","reactjs",
-				"larvel","postgres","dart","flutter"]
+				"larvel","postgres","dart","flutter",
+				"durple","java","spring","spring boot",
+				"boot","css","bootstrap","tailwind","html"
+				"html5","saas","node"]
 
 df=pd.read_csv('msgs_dataset.csv')
 
 for i in range(0,100):
-	print(type(df["message"][i]))
+	print((df["message"][i].find("#Software_design_and_Development"))
