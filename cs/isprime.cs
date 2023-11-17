99740c59c19c5ec3189b3b62024b36e7ae69a0f4
YabseraBogale <yabserabogalercd30932013@gmail.com>
2023-11-17 19:06:30 +0300

    ok

--- cs/isprime.cs
+++ cs/isprime.cs
@@ -7,13 +7,14 @@ public class HelloWorld
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine ("Hello Mono World");
+        
     }
     static int[]giveFive(int number,out int sum,ref bool three){
         int[] array=new int[5];
         bool stop=false;
         int i=1;
         int k=0;
+        bool isprime=true;
         sum=0;
         while(stop!=true){
             int isnumber=number+i;
@@ -22,22 +23,34 @@ public class HelloWorld
             } else if(isnumber==2){
                 if(k<5){
                     a[k]=isnumber;
+                    sum+=isnumber;
                     k+=1;
                     if(k==5){
                         stop=true;
                     }
                 }
             } else if(isnumber%2!=0){
-                bool isprime=true;
                 for(int j=2;j<(int)Math.Sqrt(isnumber);j++){
                     if(isnumber%j==0){
                         isprime=false;
+                        break;
+                    }
+                }
+            }
+            if(isprime==true){
+                if(k<5){
+                    a[k]=isnumber;
+                    sum+=a[k];
+                    k+=1;
+                    if(k==5){
+                        stop=true;
                     }
                 }
             }
             
         }
-        
+        three=sum%3==0;
+        return array;
         
     }
 }
