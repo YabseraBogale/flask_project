328f3f71591289047b385e481eeadd0346e5109c
YabseraBogale <yabserabogalercd30932013@gmail.com>
2023-11-21 17:11:13 +0300

    ok

--- cs/AllCpuSchedule.cs
+++ cs/AllCpuSchedule.cs
@@ -10,6 +10,8 @@ public class Program
     int StartTime;
     int Priority;
     int Remain;
+    int[] InteraptionTime;
+    int InteraptionTimeCount;
     Random numbers;
     string ProcessName;
     int BurstTime;
@@ -21,6 +23,13 @@ public class Program
         this.WaitingTime=StartTime-ArrivalTime;
         this.ProcessName=ProcessName;
     }
+
+    public void SetInteraptionTime(params int[] InteraptionTime){
+        this.InteraptionTime=new int[InteraptionTime.Length];
+        this.InteraptionTime=InteraptionTime;
+        this.InteraptionTimeCount=InteraptionTime.Length;
+    }
+
     public int[] GivingRepeateRandomNumber(int LengthOfRandomNumber,int MaxRange=10,bool Repeat=false){
       this.RandomNumbers=new int[LengthOfRandomNumber];
       for(int i=0;i<LengthOfRandomNumber;i++){
@@ -37,10 +46,15 @@ public class Program
       }
       return this.RandomNumbers;
     }
+    
     public int GetArrivalTime(){ return this.ArrivalTime; }
+    
     public int GetWaitingTime(){ return this.WaitingTime; }
+    
     public int GetStartTime(){ return this.StartTime; }
+    
     public string GetProcessName(){ return this.ProcessName; }
+  
   }
 
   public static void Main()
