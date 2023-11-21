1ae015e872f1b4949c28721088ce26759314f1ff
YabseraBogale <yabserabogalercd30932013@gmail.com>
2023-11-21 18:02:26 +0300

    ok

--- cs/AllCpuSchedule.cs
+++ cs/AllCpuSchedule.cs
@@ -21,7 +21,7 @@ public class Program
 
     }
 
-    Process(string ProcessName,int ArrivalTime,int StartTime,int BurstTime,int Priority=0,int TimeSlot=0,int StopTime=0){
+    public Process(string ProcessName,int ArrivalTime,int StartTime,int BurstTime,int Priority=0,int TimeSlot=0,int StopTime=0){
         this.ArrivalTime=ArrivalTime;
         this.BurstTime=BurstTime;
         this.StartTime=StartTime;
@@ -72,32 +72,7 @@ public class Program
     public int GetRemain(){ return this.Remain; }
   }
 
-  void SortBasedOnArrivalTime(ref Process[] p){
-    for(int i=0;i<p.Length;i++){
-        for(int j=0;j<i;i++){
-            if(p[i].GetArrivalTime()<p[j].GetArrivalTime()){
-                Process temp=new Process();
-                temp=p[j];
-                p[j]=p[i];
-                p[i]=temp;
-            }
-        }
-    }
-  }
-
-  void SortBasedOnPriority(ref Process[] p){
-    for(int i=0;i<p.Length;i++){
-        for(int j=0;j<i;i++){
-            if(p[i].GetPriority()<p[j].GetPriority()){
-                Process temp=new Process();
-                temp=p[j];
-                p[j]=p[i];
-                p[i]=temp;
-            }
-        }
-    }
-  }
-
+  
   public static void Main()
   {
   
