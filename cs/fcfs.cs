64ae592af4fcef8f43427a6fde5af911d04d749e
YabseraBogale <yabserabogalercd30932013@gmail.com>
2023-11-21 15:26:21 +0300

    ok

--- cs/fcfs.cs
+++ cs/fcfs.cs
@@ -25,15 +25,21 @@ public class Program
   }
   public static void main(){
     
-    int[] numbers=GiveMeNoRepateRandomNumber(5,10);
+    int[] numbers=GiveMeNoRepateRandomArrayNumber(5,10);
     Process[] process= new Process[numbers.Length];
     Random number=new Random();
     for(int i=0;i<process.Length;i++){
       process[i]=new Process();
       process[i].setAll(i.ToString(),numbers[i],numbers[i]*number.Next(numbers[i]));
     }
+    foreach(Process i in process ){
+        Console.WriteLine("Process-Name: {0}, Arrival-Time: {1}, Burst-Time: {2}",i.getProcessName(),i.getArravelTime(),i.getBurstTime());
+    }
   }
-  static int[] GiveMeNoRepateRandomNumber(int length,int maxRange){
+
+
+
+  static int[] GiveMeNoRepateRandomArrayNumber(int length,int maxRange){
       int[] arr =new int[length];
       Random numbers=new Random();
       for(int i=0;i<length;i++){
