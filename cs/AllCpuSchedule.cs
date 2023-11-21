1f2b527e4ab0b5fbe88927e418935283875a8c29
YabseraBogale <yabserabogalercd30932013@gmail.com>
2023-11-21 18:30:29 +0300

    ok

--- cs/AllCpuSchedule.cs
+++ cs/AllCpuSchedule.cs
@@ -23,12 +23,12 @@ public class Program
 
     }
 
-    public Process(string ProcessName,int ArrivalTime,int StartTime,int BurstTime,int Priority=0,int TimeSlot=0,int StopTime=0){
+    public Process(int ProcessName,int ArrivalTime,int StartTime,int BurstTime,int Priority=0,int TimeSlot=0,int StopTime=0){
         this.ArrivalTime=ArrivalTime;
         this.BurstTime=BurstTime;
         this.StartTime=StartTime;
         this.WaitingTime=StartTime-ArrivalTime;
-        this.ProcessName=ProcessName;
+        this.ProcessName="P"+ProcessName.ToString();
         this.Priority=Priority;
         this.TimeSlot=TimeSlot;
         this.StopTime=StopTime;
@@ -46,7 +46,24 @@ public class Program
         this.Done=Done;
     }
 
-    public int[] GivingRepeateRandomNumber(int LengthOfRandomNumber,int MaxRange=10,bool Repeat=false){
+    public int[] GetRepeateRandomNumber(int LengthOfRandomNumber,int MaxRange=10,bool Repeat=false){
+      this.RandomNumbers=new int[LengthOfRandomNumber];
+      for(int i=0;i<LengthOfRandomNumber;i++){
+          int number=this.numbers.Next(MaxRange);
+          if(Repeat==false){
+              if(this.RandomNumbers.Contains(number)==false){
+                this.RandomNumbers[i]=number;
+              } else{
+                i-=1;
+              }
+          } else{
+            this.RandomNumbers[i]=number;
+          }
+      }
+      return this.RandomNumbers;
+    }
+
+    public int[] GetRepeateRandomNumber(int LengthOfRandomNumber,bool Repeat=false,int MaxRange=10){
       this.RandomNumbers=new int[LengthOfRandomNumber];
       for(int i=0;i<LengthOfRandomNumber;i++){
           int number=this.numbers.Next(MaxRange);
@@ -110,6 +127,15 @@ public class Program
             }
         }
     }
+  }
+
+  Process[] MakeProcess(int NumberOfProcess,bool Repeat=false){
+      Process[] p= new Process[NumberOfProcess];
+      Process temp=new Process();
+      int[] RandomArrivalTime=temp.GetRepeateRandomNumber(NumberOfProcess,Repeat);
+      int[] RandomBurstTime=temp.GetRepeateRandomNumber(NumberOfProcess);
+      return p;
+
   }
 
   public static void Main()
