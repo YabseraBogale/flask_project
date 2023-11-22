bb8cb6a2313f317c64b50a32e21beca06d284c2d
YabseraBogale <yabserabogalercd30932013@gmail.com>
2023-11-22 17:04:29 +0300

    ok

---
+++ cs/gg.cs
@@ -0,0 +1,81 @@
+you life, [11/22/23 5:00 PM]
+[Forwarded from Miki D]
+//C# equivalents
+
+using System;
+
+namespace Main
+{
+class Program
+{
+  static void Main(string[] args)
+  {
+  // Matrix for storing Process Id, Burst
+  // Time, Average Waiting Time & Average
+  // Turn Around Time.
+  int[,] A = new int[100, 4];
+  int n;
+  int total = 0;
+  float avg_wt, avg_tat;
+  Console.WriteLine("Enter number of process:");
+  n = Convert.ToInt32(Console.ReadLine());
+  Console.WriteLine("Enter Burst Time:");
+  for (int i = 0; i < n; i++)
+  {
+    // User Input Burst Time and alloting
+    // Process Id.
+    Console.Write("P" + (i + 1) + ": ");
+    A[i, 1] = Convert.ToInt32(Console.ReadLine());
+    A[i, 0] = i + 1;
+  }
+  for (int i = 0; i < n; i++)
+  {
+    // Sorting process according to their
+    // Burst Time.
+    int index = i;
+    for (int j = i + 1; j < n; j++)
+    {
+    if (A[j, 1] < A[index, 1])
+    {
+      index = j;
+    }
+    }
+    int temp = A[i, 1];
+    A[i, 1] = A[index, 1];
+    A[index, 1] = temp;
+    temp = A[i, 0];
+    A[i, 0] = A[index, 0];
+    A[index, 0] = temp;
+  }
+  A[0, 2] = 0;
+  // Calculation of Waiting Times
+  for (int i = 1; i < n; i++)
+  {
+    A[i, 2] = 0;
+    for (int j = 0; j < i; j++)
+    {
+    A[i, 2] += A[j, 1];
+    }
+    total += A[i, 2];
+  }
+  avg_wt = (float)total / n;
+  total = 0;
+  // Calculation of Turn Around Time and printing the
+  // data.
+  Console.WriteLine("P\tBT\tWT\tTAT");
+  for (int i = 0; i < n; i++)
+  {
+    A[i, 3] = A[i, 1] + A[i, 2];
+    total += A[i, 3];
+    Console.WriteLine("P" + A[i, 0] + "\t"
+            + A[i, 1] + "\t" + A[i, 2]
+            + "\t" + A[i, 3]);
+  }
+  avg_tat = (float)total / n;
+  Console.WriteLine("Average Waiting Time= "
+            + avg_wt);
+  Console.WriteLine("Average Turnaround Time= "
+            + avg_tat);
+  }
+}
+}
