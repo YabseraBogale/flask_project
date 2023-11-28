2884bbc2ff6f1fc34b5002f0986ea771434c84f9
YabseraBogale <yabserabogalercd30932013@gmail.com>
2023-11-28 17:42:37 +0300

    i don't know what i have become

--- reading-json/test.py
+++ reading-json/test.py
@@ -9,12 +9,13 @@ test=Software()
 lst=test.SeeInMessage("Verified")
 Compaines={}
 for i in range(0,100):
-    name=lst[i].split("__________________")[1].split("[Verified")[0].strip().lower()
+    name=lst[i].split("__________________")[1].split("[Verified")[0].strip().upper()
     if name not in Compaines:
         Compaines[name]=1
     else:
         Compaines[name]+=1
     
+k=0
 
-
-pprint(Compaines)
+for name,count in Compaines.items():
+    print(test.InsertCompaines(name,count))
