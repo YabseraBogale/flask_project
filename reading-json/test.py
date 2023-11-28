1275a3b99d34a96a67454df5077e2ed308fdf83f
YabseraBogale <yabserabogalercd30932013@gmail.com>
2023-11-28 17:37:44 +0300

    i don't know what i have become

--- reading-json/test.py
+++ reading-json/test.py
@@ -1,15 +1,20 @@
 from database import Software
 from random import randint
+from pprint import pprint
 test=Software()
 
 
-"""
+
 
 lst=test.SeeInMessage("Verified")
-
+Compaines={}
 for i in range(0,100):
-    print(lst[i].split("__________________")[1].split("[Verified")[0].strip())
+    name=lst[i].split("__________________")[1].split("[Verified")[0].strip().lower()
+    if name not in Compaines:
+        Compaines[name]=1
+    else:
+        Compaines[name]+=1
     
 
 
-"""
+pprint(Compaines)
