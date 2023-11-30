d762b16f597cc2111e73767dd386055834051b44
YabseraBogale <yabserabogalercd30932013@gmail.com>
2023-11-30 17:16:59 +0300

    ok

--- reading-json/main.py
+++ reading-json/main.py
@@ -1,9 +1,15 @@
+"""
+    the project have to many fault thinking of restarting it.
+
+"""
+
 import pandas as pd
 from time import sleep
 from pprint import pprint
 from database import Software
 import json
 import os
+
 
 
 list_of_stack=["mysql","go","python","c++","javascript",
@@ -37,15 +43,33 @@ for i in range(0,len(df["message"])):
             Compaine[name]=1
             count+=1
         else:
-            Compaine[name]=1
+            Compaine[name]+=1
             count+=1
         if count==100:
             print("At index",i)
             count=0
             sleep(3)
-     
 
-
+os.system("clear")
+print("Inserting to Database")
+count=0    
+for i in range(0,len(df["message"])):
+	
+	if str(df["message"][i]).find("#Software_design_and_Development")!=-1:
+		table.insertIntoTable(i,df["message"][i],df["date"][i])
+	elif table.checkAllWith(list_of_stack,df["message"][i])==True:
+		table.insertIntoTable(i,df["message"][i],df["date"][i])
+	elif table.checkAllWith(list_of_job,df["message"][i])==True:
+		table.insertIntoTable(i,df["message"][i],df["date"][i])
+		
+	if count==100:
+		count=0
+		print(f"Insert up to {i}")
+		sleep(3)
+	else:
+		count+=1
+          
+os.system("clear")
 print("Inserting to Database")
 count=0
 for Name,Request in Compaine.items():
--- reading-json/test.py
+++ reading-json/test.py
@@ -2,6 +2,4 @@ from database import Software
 from random import randint
 from pprint import pprint
 test=Software()
-test.DropCompainesTable()
-test.DropErrTable()
 test.createTable()
