04c62f2cda1da86c4aa933962b9440064bd32322
YabseraBogale <yabserabogalercd30932013@gmail.com>
2023-12-07 18:06:24 +0300

    ok

--- freelanceEthiopa/database.sql
+++ freelanceEthiopa/database.sql
@@ -6,8 +6,11 @@ create table Software(
 
 create table Compaine(
 	CompanyName varchar(30) not null,
+	JobTitle varchar(30) not null,
 	location varchar(30) not null,
 	NumberOfRequest int not null,
 	Date datetime not null,
+	TechnologyStack json not null,
+
 	
 );
