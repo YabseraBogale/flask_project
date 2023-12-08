5039c15bb1be526c05ca920ad1494995547dc895
YabseraBogale <yabserabogalercd30932013@gmail.com>
2023-12-08 17:31:38 +0300

    ok

--- Vaccum Cleaner/studentSimpleAgent.py
+++ Vaccum Cleaner/studentSimpleAgent.py
@@ -3,7 +3,7 @@ class SimpleStudentAgent():
 		self.Id=["RCD/3093/2013","RCD/3091/2013","RCD/3087/2013","RCD/3053/2013","RCD/3120/2013"]
 		self.Student="out"
 	def Preciver(self,ID):
-		if self.Id.index(ID)!=-1:
+		if self.Id.count(ID)==1:
 			self.Student="in"
 		else:
 			self.Student="out"
