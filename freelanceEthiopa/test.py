b8cd0fef904ca6186af647fce2cfc83b41881710
YabseraBogale <yabserabogalercd30932013@gmail.com>
2023-12-10 14:20:43 +0300

    ok

--- freelanceEthiopa/test.py
+++ freelanceEthiopa/test.py
@@ -1,8 +1,11 @@
 from pprint import pprint
 from database import Database,StackOfTechnology
 from time import sleep
-
+count=0
 test=Database()
 lst=test.SeeAllCompanyTitle()
 for i in lst:
-    pprint(str(i))
+    pprint(i)
+    if count==5:
+        break
+    count+=1
