af62fac9bdf1a9b8b3eaf38a6f4edd5b05353a7d
YabseraBogale <yabserabogalercd30932013@gmail.com>
2023-12-10 14:17:14 +0300

    ok

--- freelanceEthiopa/test.py
+++ freelanceEthiopa/test.py
@@ -3,10 +3,3 @@ from database import Database,StackOfTechnology
 from time import sleep
 
 test=Database()
-phone=test.CheckInMessageSoftware("Phone")
-location=test.CheckInMessageSoftware("location")
-phoneandlocation=test.GetLocationAndPhoneFromSoftWare()
-
-print("Number of phone number",len(phone))
-print("Number of location",len(location))
-print("Both together",len(phoneandlocation))
