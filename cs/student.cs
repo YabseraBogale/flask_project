91b38787be2004658c6ed10f3132037d1c0c184b
YabseraBogale <yabserabogalercd30932013@gmail.com>
2023-12-15 19:22:52 +0300

    ok

---
+++ cs/student.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Program
+{
+  class School{
+    string FirstName="";
+    string LastName="";
+    string MiddleName="";
+    public School(){
+
+    }
+  }
+  public static void Main()
+  {
+  }
+}
+
