7699c22dab4ee5d0f682ce0b812e77780365c1cb
YabseraBogale <yabserabogalercd30932013@gmail.com>
2023-12-16 18:38:09 +0300

    ok

---
+++ cs/Course.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Program
+{
+  class Course{
+    float test;
+    float mid;
+    float final;
+    protected float Test{
+      get{return test;}
+      set{
+        test=value;
+      }
+    }
+    protected float Mid{
+      get{return mid;}
+      set{
+        mid=value;
+      }
+    }
+    protected float Final{
+      get{return final;}
+      set{
+        final=value;
+      }
+    }
+    
+
+
+  }
+  public static void Main()
+  {
+  
+  }
+}
+
