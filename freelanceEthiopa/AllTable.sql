a03f9d7f035642f84af39e6b65eba082ffb8434f
YabseraBogale <yabserabogalercd30932013@gmail.com>
2023-12-17 15:11:41 +0300

    ok

--- freelanceEthiopa/AllTable.sql
+++ freelanceEthiopa/AllTable.sql
@@ -1,6 +1,8 @@
 -- blue print for the all channel tables and thier attribute
 create table channelName(
+    CompanyName varchar(20) not null,
     RequestData datetime not null,
     Location varchar(20) not null,
     JobPosition varchar(20) not null,
+    Salary floatnot not null,
 );
