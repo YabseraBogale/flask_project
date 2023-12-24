af18c047273df86a0ed496971abb5a2bfe63bd13
YabseraBogale <yabserabogalercd30932013@gmail.com>
2023-12-24 16:34:20 +0300

    ok

--- Api/main.py
+++ Api/main.py
@@ -9,5 +9,5 @@ name=CompanyName().GiveCompanyName()
 
 
 for i in range(0,5):
-    print(i)
+    print(title[i])
 
--- Api/prime.py
+++ Api/prime.py
@@ -3,6 +3,7 @@ class Prime():
     def __init__(self):
         self.connection=sqlite3.Connection("prime.db")
         self.pointer=self.connection.cursor()
+
     def CreateTable(self):
         statment="create table IF NOT EXISTS create table SearchTermTitle( title varchar(30) not null);"
         self.pointer.execute(statment)
@@ -11,29 +12,37 @@ class Prime():
         self.pointer.execute(statment)
         self.connection.commit()
         return "Table Created"
-    def InsertIntoSearchTerm(self,title,companyname):
+
+    def InsertIntoSearchTermTitle(self,title):
         statment="insert into SearchTermTitle(title) values(?)"
         self.pointer.execute(statment,(title,))
         self.connection.commit()
+        return f"Successful into Title: {title}"
+
+    def InsertIntoSearchTermCompanyName(self,name):
         statment="insert into SearchTermCompanyName(companyname) values(?)"
         self.pointer.execute(statment,(companyname,))
         self.connection.commit()
-        return f"Successful Inserted title: {title} name: {companyname}"
+        return f"Successful into Company Name: {companyname}"
+
     def AllGetCompanyName(self):
         statment="select companyname from SearchTermCompanyName"
         self.pointer.execute(statment)
         result=self.pointer.fetchall()
         return result
+
     def AllGetTitle(self):
         statment="select title from SearchTermTitle"
         self.pointer.execute(statment)
         result=self.pointer.fetchall()
         return result
+
     def IsTitleInSearchTerm(self,title):
         statment="select title from SearchTermTitle where title=?"
         self.pointer(statment,(title,))
         result=self.pointer.fetchall()
         return result
+
     def IsCompanyNameInSearchTerm(self,companyname):
         statment="select companyname from SearchTermCompanyName where=?"
         self.pointer(statment,(companyname,))
