da79bcf9bd63b45d4290eb371ca7a1ca0675a684
YabseraBogale <yabserabogalercd30932013@gmail.com>
2023-12-25 12:24:40 +0300

    ok

--- Api/database.py
+++ Api/database.py
@@ -11,5 +11,15 @@ class Database():
         statment="create table IF NOT EXISTS CompanyInformation( companyname varchar(80) not null, telegramchannel varchar(20) not null, title varchar(30) not null,date datetime not null,location(50) not null, techstackid int not null )"
         self.pointer.execute(statment)
         self.connection.commit()
-
-    def InsertIntoTechStack(self,filename)
+        return "Sucessfully made table"
+    
+    def InsetIntoCompanyInformation(self,name,telegramchannel,title,date,location,techstackid):
+        statment="insert into CompanyInformation(companyname,telegramchannel,title,date,location,techstackid) values(?,?,?,?,?,?)"
+        self.pointer.execute(statment,(name,telegramchannel,title,date,location,techstackid))
+        self.connection.commit()
+        return "Scessfully inserted into table Company infromation"
+    
+    def InsertIntoTechStack(self,id,filename):
+        statment="insert into TechStack(id,filename) values(?,?)"
+        self.pointer.execute(statment,(id,filename))
+        self.connection.commit()
