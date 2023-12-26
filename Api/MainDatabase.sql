c6d2f5d62cd5538c74aba96267e4eca7fc0e1c52
YabseraBogale <yabserabogalercd30932013@gmail.com>
2023-12-26 12:21:33 +0300

    ok

--- Api/MainDatabase.sql
+++ Api/MainDatabase.sql
@@ -4,15 +4,7 @@ create table CompanyInformation(
     title varchar(30) not null,
     date datetime not null,
     location(50) not null,
-    techstackid int not null,
-    foreign key (techstackid) references TechStack(id)
-)
-
-insert into CompanyInformation(companyname,telegramchannel,title,date,location,techstackid) values(?,?,?,?,?,?)
-
-create table SearchTermTechStack(
-    id int not null primary key,
     stack json
 )
 
-insert into TechStack(id,stack) values(?,?)
+insert into CompanyInformation(companyname,telegramchannel,title,date,location,stack) values(?,?,?,?,?,?)
