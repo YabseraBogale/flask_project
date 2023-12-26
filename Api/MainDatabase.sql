fe5c4666b39f33d2378701f19c6bac13aeb9e17e
YabseraBogale <yabserabogalercd30932013@gmail.com>
2023-12-26 18:10:08 +0300

    ok

--- Api/MainDatabase.sql
+++ Api/MainDatabase.sql
@@ -1,5 +1,5 @@
-create table CompanyInformation(
-    companyname varchar(80) not null, 
+create table IF NOT EXISTS CompanyInformation(
+    companyname varchar(80) not null,
     telegramchannel varchar(20) not null unique,
     title varchar(30) not null,
     date datetime not null,
@@ -7,4 +7,12 @@ create table CompanyInformation(
     stack json
 )
 
+create table IF NOT EXISTS MetaData(
+    nameofchannel varchar(80) not null,
+    sizeofdataset int not null,
+    foundjob int not null,
+    perectange float not null
+)
+
 insert into CompanyInformation(companyname,telegramchannel,title,date,location,stack) values(?,?,?,?,?,?)
+insert into MetaData(nameofchannel,sizeofdataset,foundjob,perectange) values(?,?,?,?)
