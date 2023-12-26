98d3af1545de530733fd0d691a9172c007d93174
YabseraBogale <yabserabogalercd30932013@gmail.com>
2023-12-26 12:23:19 +0300

    ok

--- Api/database.py
+++ Api/database.py
@@ -5,10 +5,8 @@ class Database():
         self.pointer=self.connection.cursor()
     
     def CreateTable(self):
-        statment="create table IF NOT EXISTS TechStack( id int not null primary key, filename varchar(20) not null )"
-        self.pointer.execute(statment)
-        self.connection.commit()
-        statment="create table IF NOT EXISTS CompanyInformation( companyname varchar(80) not null, telegramchannel varchar(20) not null, title varchar(30) not null,date datetime not null,location(50) not null, techstackid int not null )"
+
+        statment="create table IF NOT EXISTS CompanyInformation( companyname varchar(80) not null, telegramchannel varchar(20) not null, title varchar(30) not null,date datetime not null,location(50) not null, stack json )"
         self.pointer.execute(statment)
         self.connection.commit()
         return "Sucessfully made table"
